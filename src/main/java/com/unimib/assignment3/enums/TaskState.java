9070a69bd1e8cd7bf830e8a2433e1b2b5b2b7934
Andex125 <andreaaivaliotis@gmail.com>
2026-01-13 23:02:47 +0100

    add comment

--- src/main/java/com/unimib/assignment3/enums/TaskState.java
+++ src/main/java/com/unimib/assignment3/enums/TaskState.java
@@ -1,7 +1,22 @@
 package com.unimib.assignment3.enums;
 
+/**
+ * The TaskState enum represents the  lifecycle stages of a task
+ */
 public enum TaskState {
+
+    /**
+     * The task has been created but work has not yet commenced.
+     */
     TO_BE_STARTED,
+
+    /**
+     * The task is currently in progress and being handled by a worker or process.
+     */
     STARTED,
+
+    /**
+     * The task has been successfully completed.
+     */
     DONE
 }
