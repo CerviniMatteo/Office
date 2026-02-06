209c805731e2266ec73e087f79d9bfe8b9999c17
CerviniMatteo <m.cervini1@campus.unimib.it>
2026-02-06 11:36:55 +0100

    Fixed out alert error

--- GUI/src/main/java/com/unimib/assignment3/UI/utils/AlertDialog.java
+++ GUI/src/main/java/com/unimib/assignment3/UI/utils/AlertDialog.java
@@ -1,14 +1,23 @@
 package com.unimib.assignment3.UI.utils;
 
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
 public class AlertDialog {
-
     public static void showAlert(String title, String message) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setContentText(message);
-        alert.showAndWait();
+        Runnable task = () -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(title);
+            alert.setHeaderText(null);
+            alert.setContentText(message);
+            alert.show();
+        };
+
+        if (Platform.isFxApplicationThread()) {
+            task.run();
+        } else {
+            Platform.runLater(task);
+        }
     }
 
 }
