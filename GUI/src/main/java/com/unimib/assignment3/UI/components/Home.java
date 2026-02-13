6395187b49c3a269729a481f08418f82c11da748
Cervini Matteo <matteo.cervini.2003@gmail.com>
2026-02-14 00:29:39 +0100

    Added add task button to home

--- GUI/src/main/java/com/unimib/assignment3/UI/components/Home.java
+++ GUI/src/main/java/com/unimib/assignment3/UI/components/Home.java
@@ -1,21 +1,43 @@
 package com.unimib.assignment3.UI.components;
 
 import com.unimib.assignment3.UI.web_socket_client.TaskWebSocketClientApp;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-
-import java.awt.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.SVGPath;
 
 import static com.unimib.assignment3.UI.components.AlertDialog.showAlert;
 
-public class Home extends HBox {
+public class Home extends StackPane {
 
     public Home() {
 
         TaskLayout tasksLayout = new TaskLayout();
         getChildren().add(tasksLayout);
-        HBox.setHgrow(tasksLayout, Priority.ALWAYS);
 
+        // Create Add Task button
+        Button addTaskButton = new Button();
+        addTaskButton.getStyleClass().add("add-task-btn");
+
+        // Create the SVG icon
+        String svgPathContent = "M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"; // Your SVG path
+        SVGPath addIcon = new SVGPath();
+        addIcon.setContent(svgPathContent);
+        addIcon.getStyleClass().add("add-icon");
+
+        // Wrap the icon in a StackPane for alignment
+        StackPane iconWrapper = new StackPane(addIcon);
+        iconWrapper.setPrefSize(24, 24); // Size of the SVG icon
+        addTaskButton.setGraphic(iconWrapper);
+
+        // Position the button at bottom-right
+        getChildren().add(addTaskButton);
+        StackPane.setAlignment(addTaskButton, Pos.BOTTOM_RIGHT);
+        StackPane.setMargin(addTaskButton, new Insets(20));
+        addTaskButton.toFront();
+
+        // Initialize WebSocket client
         TaskWebSocketClientApp webSocketClientApp = new TaskWebSocketClientApp(tasksLayout);
         try {
             webSocketClientApp.start();
@@ -24,4 +46,3 @@ public class Home extends HBox {
         }
     }
 }
-
